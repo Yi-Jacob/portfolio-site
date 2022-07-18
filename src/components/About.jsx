1e58801c18d5f8a4875955c513c4ab9068c8ca16
Yi-Jacob <jakeyi00@gmail.com>
2022-07-17 22:35:58 -0700

    in progress

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import ReactMarkdown from 'react-markdown';
-import { Container, Col, Row } from 'react-bootstrap';
+import { Card, Container, Col, Row } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import Fade from 'react-reveal';
 import Header from './Header';
 import endpoints from '../constants/endpoints';
 import FallbackSpinner from './FallbackSpinner';
+import { ThemeContext } from 'styled-components';
 
 const styles = {
   introTextContainer: {
@@ -25,6 +26,7 @@ const styles = {
 };
 
 function About(props) {
+  let theme = useContext(ThemeContext);
   const { header } = props;
   const [data, setData] = useState(null);
 
@@ -44,20 +46,50 @@ function About(props) {
   }, []);
 
   return (
+
     <>
       <Header title={header} />
-      <div className="section-content-container">
+
+      <div className="section-content-container1">
         <Container>
           {data
             ? (
               <Fade>
                 <Row>
-                  <Col style={styles.introTextContainer}>
+                  <Col>
+                    <Card
+                      style={{
+                        ...styles.cardStyle,
+                        backgroundColor: theme.cardBackground,
+                        borderColor: theme.cardBorderColor,
+                      }}
+                      text={theme.bsSecondaryVariant}
+                    >
+                      <div className="row">
+                        <div className="col-md-9">
+                          <Card.Body>
+                            <Card.Title style={styles.cardTitleStyle}>Jacob Yi</Card.Title>
+                            <Card.Text style={styles.cardTextStyle} style={{ textAlign: 'left' }}>
+                              {parseIntro(data.about)}
+                            </Card.Text>
+
+                          </Card.Body>
+
+                        </div>
+                        <div className="col-md-3">
+                          <Card.Img src={data?.imageSource} alt="profile" />
+                        </div>
+                      </div>
+                      </Card>
+
+                  </Col>
+
+                  {/* <Col style={styles.introTextContainer}>
                     {parseIntro(data.about)}
                   </Col>
                   <Col style={styles.introImageContainer}>
                     <img src={data?.imageSource} alt="profile" />
-                  </Col>
+                  </Col> */}
                 </Row>
               </Fade>
             )
