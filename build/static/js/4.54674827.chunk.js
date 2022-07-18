e26ae01b596038c96a08207afb3cde578dd35816
Yi-Jacob <jakeyi00@gmail.com>
2022-07-18 08:59:36 -0700

    changed icon

---
+++ build/static/js/4.54674827.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[4,8],{156:function(t,i,e){},87:function(t,i,e){"use strict";e.r(i);e(0),e(89);var n=e(1);i.default=function(t){var i=t.title;return Object(n.jsx)("div",{className:"header",children:i})}},92:function(t,i,e){"use strict";e.r(i);e(10),e(0),e(155),e(142),e(63),e(18),e(24),e(87),e(29),e(156),e(1)}}]);
+//# sourceMappingURL=4.54674827.chunk.js.map
--- build/static/js/4.fd4976e1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpportfolio-app"]=this["webpackJsonpportfolio-app"]||[]).push([[4,8],{155:function(t,i,e){},87:function(t,i,e){"use strict";e.r(i);e(0),e(89);var n=e(1);i.default=function(t){var i=t.title;return Object(n.jsx)("div",{className:"header",children:i})}},92:function(t,i,e){"use strict";e.r(i);e(10),e(0),e(154),e(142),e(63),e(19),e(24),e(87),e(29),e(155),e(1)}}]);
-//# sourceMappingURL=4.fd4976e1.chunk.js.map
